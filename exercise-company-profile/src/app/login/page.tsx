89771e7e5aa51d241d4abc8b991437a724ba7eb3
FlazeFy <flazen.edu@gmail.com>
2025-11-16 23:54:46 +0700

    [Exercise Company Profile] Login Form (Guest) Section

---
+++ exercise-company-profile/src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import OrganismsLoginForm from "@/components/organisms/o_login_form";
+
+export default function LoginPage() {
+    return (
+        <div className="flex flex-col min-h-screen p-5 lg:p-10 lg:max-w-[1440px] lg:mx-auto">
+            <OrganismsLoginForm/>
+        </div>
+    );
+}
---
+++ exercise-company-profile/src/components/organisms/o_login_form.tsx
@@ -0,0 +1,107 @@
+"use client"
+import axios from "axios"
+import * as React from "react"
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as Yup from "yup"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Form, FormField, FormItem, FormLabel, FormControl, FormMessage } from "@/components/ui/form"
+import Swal from "sweetalert2"
+import { useRouter } from "next/navigation"
+import Link from 'next/link'
+import { useAuthStore } from "@/store/authStore"
+
+const loginSchema = Yup.object({
+    email: Yup.string().required("Email is required").min(10).test(
+        "is-gmail",
+        "Email must end with @gmail.com",
+        (val: string) => val ? val.endsWith("@gmail.com") : false
+    ),
+    password: Yup.string().required("Password is required").min(6),
+})
+
+type LoginFormValues = Yup.InferType<typeof loginSchema>
+
+const OrganismsLoginForm: React.FC = () => {
+    const router = useRouter()
+    const form = useForm<LoginFormValues>({ resolver: yupResolver(loginSchema), defaultValues: { email: "", password: "" }})
+    const setEmail = useAuthStore((state: any) => state.setEmail)
+
+    const onSubmit = async (values: LoginFormValues) => {
+        try {
+            const res = await axios.get(`https://saintlyneed-us.backendless.app/api/data/guest?where=%60email%60%20%3D%20'${values.email}'%20AND%20%60password%60%20%3D%20'${values.password}'`);
+            
+            if(res && res.data.length > 0){
+                const email = res.data[0].email
+
+                Swal.fire({
+                    icon: "success",
+                    title: "Done",
+                    text: `Welcome back guest ${email}!`,
+                    confirmButtonText: "Explore now!",
+                    allowOutsideClick: false,
+                    allowEscapeKey: false,
+                }).then((result:any) => {
+                    setEmail(email.replace("@gmail.com",""))
+                    router.push("/")
+                })
+            } else {
+                Swal.fire({
+                    icon: "error",
+                    title: "I'm sorry",
+                    text: "Your username or password incorrect",
+                })
+            }
+        } catch (err: any) {
+            Swal.fire({
+                icon: "error",
+                title: "I'm sorry",
+                text: err.response.data.message,
+            })
+        }
+    }
+
+    return (
+        <div className="container rounded-2xl space-y-4 m-5 p-5 lg:m-10 lg:p-10 shadow-2xl" style={{ maxWidth: "480px" }}>
+            <h1 className="font-bold">Welcome to Vorta Beauty Clinic</h1>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                    <FormField control={form.control} name="email"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Email Address</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Enter your email" {...field} />
+                                </FormControl>
+                                <FormMessage>{form.formState.errors.email?.message}</FormMessage>
+                            </FormItem>
+                        )}
+                    />
+                    <FormField control={form.control} name="password"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <Input type="password" placeholder="Enter your password" {...field} />
+                                </FormControl>
+                                <FormMessage>{form.formState.errors.password?.message}</FormMessage>
+                            </FormItem>
+                        )}
+                    />
+                    <Button type="submit" className="w-full" disabled={form.formState.isSubmitting}>
+                        {
+                            form.formState.isSubmitting ? "Validate your account..." : "Sign In"
+                        }
+                    </Button>
+                </form>
+            </Form>
+            <hr className="mt-5"></hr>
+            <Link href="/signup">
+                <Button variant="link" className="w-full">Don't have account? Register Now</Button>
+            </Link>
+        </div>
+    )
+}
+
+export default OrganismsLoginForm
