bd0ba702eec1601d48cfc2dea46915e116070a6a
FlazeFy <flazen.edu@gmail.com>
2025-11-16 23:41:24 +0700

    [FE] Register Form (Guest) Section

---
+++ exercise-company-profile/src/app/register/page.tsx
@@ -0,0 +1,9 @@
+import OrganismsRegisterForm from "@/components/organisms/o_register_form";
+
+export default function RegisterPage() {
+    return (
+        <div className="flex flex-col min-h-screen p-5 lg:p-10 lg:max-w-[1440px] lg:mx-auto">
+            <OrganismsRegisterForm/>
+        </div>
+    );
+}
